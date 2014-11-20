67c8bc675e8dded3cde76c9f36cd3fffaf778682
hettema <hettema@gmail.com>
2014-11-20 21:04:14 +0100

    created a nukeProject method (sans security) which destroys the project, all associated screens as well associated userStories. I AM A GOD :)

--- client/templates/projects/project_list.js
+++ client/templates/projects/project_list.js
@@ -1,22 +1,19 @@
 Template.projectList.helpers({
-	projects: function () {
-		return Projects.find()
-	},
-	removeProject: function (){
-		this.remove();
-	}
+    projects: function() {
+        return Projects.find()
+    },
+    removeProject: function() {
+        this.remove();
+    }
 });
 
 Template.projectList.events({
-	'click #delete': function (e, template) {
-		e.preventDefault();
+    'click #delete': function(e, template) {
+        e.preventDefault();
 
-		if(confirm("You sure? \n1. this cannot be un-done\n2. it's permanent")) {
-			var project = this._id;
-			Screens.remove({project_id: this._id}) // remove the screens
-			Userstories.remove({project_id: this._id}) // remove the screens
-			Projects.remove(project); // remove the project
-		}
-		
-	}
+        if (confirm("You sure? \n1. this cannot be un-done\n2. it's permanent")) {
+            Meteor.call("nukeProject", this._id);
+            
+        }
+    }
 });
