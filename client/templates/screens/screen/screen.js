075dd7580abcfc61230df29fb95e13d49d65a182
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-11-30 15:58:08 +0200

    handle blank string, returning to template.gind

--- client/templates/screens/screen/screen.js
+++ client/templates/screens/screen/screen.js
@@ -1,24 +1,22 @@
 Template.screen.helpers({
-	listUserStories: function () {
-		var id = this._id // grab the ID of the current screen
-		return Userstories.find({screen_id: id}) // show the userstories associated to this screen
-	}
+    listUserStories: function() {
+        var id = this._id // grab the ID of the current screen
+        return Userstories.find({
+                screen_id: id
+            }) // show the userstories associated to this screen
+    }
 });
 
 Template.screen.events({
     'click .renameScreen': function(e, template) {
         e.preventDefault();
 
-        var new_title = prompt("new title please");
-
-        // PURE JS DOESN'T WORK HERE? WHY?
-        var collection = $(".renameScreen").data("collection");
-
-        // template.find("#rename").dataset.collection;
+        // returning title on false or blank string
+        var new_title = (prompt("new title please") || this.title),
+            collection = $(template.find(".renameScreen")).data('collection')
 
         Meteor.call("rename", collection, this, new_title)
-
-        }
+    }
 });
 
 Template.screen.events({
@@ -34,21 +32,21 @@ Template.screen.events({
 
         Meteor.call("rename", collection, this, new_title)
 
-        }
+    }
 });
 
 
 
 Template.projectPage.events({
-    'click .toggleStory': function (e, template) {
+    'click .toggleStory': function(e, template) {
         e.preventDefault();
 
         // check which story was clicked
         var story_id = this._id;
         var screen_id = this.screen_id;
-        
+
         Meteor.call('highlightStory', story_id, screen_id);
-        
-        
+
+
     }
 });
