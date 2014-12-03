4793eecca3aa4442f6dfabb59059d580bc1435b7
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-12-03 12:17:16 +0000

    move projectpage events to correct place

--- client/templates/projects/project_page/project_page.js
+++ client/templates/projects/project_page/project_page.js
@@ -22,3 +22,27 @@ Template.projectPage.helpers({
 	}
 });
 
+Template.projectPage.events({
+    'click .toggleStory': function (e, template) {
+        e.preventDefault();
+
+        // check which story was clicked
+        var story_id = this._id;
+        var screen_id = this.screen_id;
+
+        if (!this.highlighted) {
+            Meteor.call('highlightToggle', story_id, screen_id)
+        } else {
+        // Meteor.call('highlightToggle', story_id, screen_id);
+        //obj = _.pluck([])
+        Meteor.call('collapseScreens', {
+          screen_id: this.screen_id,
+          highlighted: this.highlighted,
+          connectsTo:this.connectsTo,
+          _id: this._id
+        })
+        }
+        //Meteor.call('collapseScreens', this)
+    }
+});
+
